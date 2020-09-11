d6c2e15ecd25fca42e4671c7104500e09385a898
Danil Vorozheykin <danilvda@gmail.com>
2020-09-11 18:50:06 +0300

    Edit Practice 9

--- n01-practice9/src/ru/mirea/n01pr9/Main.java
+++ n01-practice9/src/ru/mirea/n01pr9/Main.java
@@ -12,10 +12,11 @@ public class Main {
 		queue.add(new Request(EmployeeCategory.M, 3, 5));
 		queue.add(new Request(EmployeeCategory.P, 4, 5));
 		queue.add(new Request(EmployeeCategory.P, 4, 15));
+		queue.add(new Request(EmployeeCategory.M, 4, 15));
 
 		Map<EmployeeCategory, Integer> timeMap = new HashMap<>();
 
-
+		System.out.println("Requests:\n");
 		while (!queue.isEmpty()) {
 			Request request = queue.peek();
 			System.out.println(request);
@@ -25,6 +26,7 @@ public class Main {
 			queue.remove();
 		}
 
+		System.out.println("\nTime needs:");
 		for (Map.Entry<EmployeeCategory, Integer> entry : timeMap.entrySet()) {
 			System.out.format(
 					"\n%s - %d minutes",
